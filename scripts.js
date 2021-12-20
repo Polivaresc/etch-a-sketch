d78ee270d62dcb26010829d2529b886b915fff54
Polivaresc <paulaolca98@gmail.com>
2021-12-20 20:21:59 +0100

    fix grid creation

--- scripts.js
+++ scripts.js
@@ -3,10 +3,16 @@ const container = document.querySelector("#container")
 const newGridBtn = document.querySelector("#newGridBtn")
 newGridBtn.addEventListener("click", newGrid)
 
-let num
+function newGrid() {
+    container.innerHTML = ""
+    const num = parseInt(prompt("Enter a number between 0 and 100: "))
+    
+    if (isNaN(num) || num > 100 || num < 0) {
+        alert("The input needs to be a number between 0 and 100")
+        return newGrid()
+    }
 
-function newGrid(num) {
-    num = prompt("Enter a number smaller than 100: ")
+    container.style["grid-template-columns"] = `repeat(${num}, auto)`
     for (i=0; i<num*num; i++) {
         const squareDiv = document.createElement("div")
         squareDiv.classList = "cell"
