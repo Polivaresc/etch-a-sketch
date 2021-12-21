bd8e9bd28b7590f2cb3b9f127e3a048617099aa2
Polivaresc <paulaolca98@gmail.com>
2021-12-21 11:59:05 +0100

    fix hover with event listener, generate random cell color

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const container = document.querySelector("#container")
 
+let brightness = 1
+
 const newGridBtn = document.querySelector("#newGridBtn")
 newGridBtn.addEventListener("click", newGrid)
 
 function newGrid() {
+    brightness = 1
     container.innerHTML = ""
     const num = parseInt(prompt("Enter a number between 0 and 100: "))
     
@@ -13,12 +16,19 @@ function newGrid() {
     }
 
     container.style["grid-template-columns"] = `repeat(${num}, auto)`
+
+    const randomColor = Math.floor(Math.random()*16777215).toString(16)
+
     for (i=0; i<num*num; i++) {
         const squareDiv = document.createElement("div")
-        squareDiv.classList = "cell"
+        squareDiv.style["background-color"] = "gainsboro"
+
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style["background-color"] = "#" + randomColor
+            squareDiv.style.filter = `brightness(${brightness})`
+            brightness = Math.max(brightness - 0.1, 0)
+        })
         container.appendChild(squareDiv)
     }
 }
-
-
 
