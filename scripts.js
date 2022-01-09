7c77f70fed38cdc9e0d779926df9d28485f5deb5
Polivaresc <paulaolca98@gmail.com>
2022-01-09 22:58:26 +0100

    brightness fixed

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@ const newGridBtn = document.querySelector("#newGridBtn")
 newGridBtn.addEventListener("click", newGrid)
 
 function newGrid() {
-    let brightness = 1
+    let arBrightness = []
+    
+
     container.innerHTML = ""
     const num = parseInt(prompt("Enter a number between 0 and 100: "))
     
@@ -19,14 +21,28 @@ function newGrid() {
 
     for (i=0; i<num*num; i++) {
         const squareDiv = document.createElement("div")
+        squareDiv.setAttribute('class', 'cell')
+        squareDiv.setAttribute('id', i)
+
+        const br = {id: i, value: 1}
+        arBrightness.push(br)
+        
         squareDiv.style["background-color"] = "gainsboro"
 
-        squareDiv.addEventListener("mouseover", () => {
-            squareDiv.style["background-color"] = "#" + randomColor
-            squareDiv.style.filter = `brightness(${brightness})`
-            brightness = Math.max(brightness - 0.1, 0)
-        })
+        
         container.appendChild(squareDiv)
     }
-}
+    
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            const id = parseInt(cell.getAttribute('id'))
+            const brightness = arBrightness.find(br => br.id === id)
+    
+            cell.style["background-color"] = "#" + randomColor
+            cell.style.filter = `brightness(${brightness.value})`
+    
+            brightness.value = Math.max(brightness.value - 0.2, 0)
+        })
+    })
 
+}
